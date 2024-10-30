d94862f6aba98ef4df251451d2ce9f1f52a34a72
BearerOP <ankity1892003@gmail.com>
2024-10-30 17:26:17 +0530

    bugs resolved

--- components/cta.tsx
+++ components/cta.tsx
@@ -177,9 +177,9 @@ export default function IconMarquee() {
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-width="2"
+            strokeWidth="2"
             stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinejoin="round"
             className="lucide lucide-heart-handshake mx-auto size-16 text-black dark:text-white lg:size-24"
           >
             <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path>
@@ -207,9 +207,9 @@ export default function IconMarquee() {
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              stroke-width="2"
+              strokeWidth="2"
               stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinejoin="round"
               className="lucide lucide-chevron-right ml-1 size-4 transition-all duration-300 ease-out group-hover:translate-x-1"
             >
               <path d="m9 18 6-6-6-6"></path>
